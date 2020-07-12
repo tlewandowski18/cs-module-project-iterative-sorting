262f39c34dd144656080245d6aea0eb485897c11
tlewandowski18 <tlewandowski18@gmail.com>
2020-07-12 19:35:21 -0400

    update searching algorithm

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,8 +1,8 @@
 def linear_search(arr, target):
     # Your code here
-    for item in arr:
-        if item == target:
-            return target
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
 
     return -1   # not found
 
@@ -11,6 +11,18 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
-
-
-    return -1  # not found
+    start = 0
+    end = len(arr) - 1
+    while end >= start:
+        #get middle point
+        middle_index = (start + end) // 2
+        #if the middle value is same as target, set found to Trun
+        if arr[middle_index] == target:
+            return middle_index
+        #move start or end index closer to the other, shrinking search space
+        else:
+            if target < arr[middle_index]:
+                end = middle_index - 1
+            if target > arr[middle_index]:
+                start = middle_index + 1
+    return -1
